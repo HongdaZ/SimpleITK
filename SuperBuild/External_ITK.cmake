cadfc8647e4893bd52c458380acd3b51547629b5
HongdaZ <zyqlxrhd@sina.cn>
2018-11-16 20:17:43 -0600

    v

--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -52,7 +52,7 @@ set(ITK_GIT_REPOSITORY "${git_protocol}://github.com/HongdaZ/ITK.git" CACHE STRI
 mark_as_advanced(ITK_GIT_REPOSITORY)
 sitk_legacy_naming(ITK_GIT_REPOSITORY ITK_REPOSITORY)
 
-set(ITK_GIT_TAG "v4.13" CACHE
+set(ITK_GIT_TAG "v4.13.0" CACHE
   STRING "Tag in ITK git repo") # release-4.13.1
 mark_as_advanced(ITK_GIT_TAG)
 set(ITK_TAG_COMMAND GIT_TAG "${ITK_GIT_TAG}")
